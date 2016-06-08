99b2185d8e1e26630cd80ee344bbc80004b8b68c
horstwurm <horst.wurm@bluewin.ch>
2016-06-08 14:12:14 +0000

    save

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,15 +4,6 @@ def index
   if user_signed_in?
     redirect_to current_user
   end
-  if params[:message] != nil
-    @message = params[:message]
-    puts "trying to send eMail with  USERID:" + ENV["GMAIL_USERNAME"] + " PASSWORD:"+ ENV["GMAIL_PASSWORD"] + "SUBJECT:" + @message
-    UserMailer.signup_confirmation(User.find(current_user.id),@message).deliver_now
-  end 
-end
-
-def dashboard
-  @events=Event.all
 end
 
 end
